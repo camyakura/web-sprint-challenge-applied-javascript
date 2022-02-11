3b4a2cb691c34df06622667737904aaa0599bb55
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-02-10 19:58:51 -0700

    card component done

--- src/components/card.js
+++ src/components/card.js
@@ -17,6 +17,27 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div');
+  const headLine = document.createElement('div');
+  const author = document.createElement('div');
+  const imageContainer = document.createElement('div');
+  const image = document.createElement('img');
+  const authorName = document.createElement('span');
+
+  card.appendChild(headLine);
+  card.appendChild(author);
+  author.appendChild(imageContainer);
+  imageContainer.appendChild(image);
+  author.appendChild(authorName);
+
+  card.classList.add('card');
+  headLine.classList.add('headline');
+  headLine.textContent = `${article.headline}`;
+  author.classList.add('author');
+  imageContainer.classList.add('img-container');
+  image.src = article.authorPhoto;
+  authorName.textContent = article.authorName;
+
 }
 
 const cardAppender = (selector) => {
